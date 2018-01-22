3333ef56f046b0b5136ee3e7b73331359d99c3f3
Ajilore Raphael Olamide <raphealolams@gmail.com>
2018-01-22 01:47:49 +0100

    mobile layout added

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import burgerLogo from '../../assets/images/burger.png';
 import classess from './Logo.css';
 const logo = (props) => (
-  <div className={classess.Logo}>
+  <div className={classess.Logo} style={{height: props.height}}>
     <img src={burgerLogo} alt="App Logo" />
   </div>
 );
--- src/components/Ui/Modal/Modal.js
+++ src/components/Ui/Modal/Modal.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styleSheet from './Modal.css'
 import Aux from '../../../hoc/New';
 import BackDrop from '../BackDrop/BackDrop';
 
-const modal = (props) => (
-  <Aux>
-    <BackDrop show={props.show} clicked={props.modalClosed}/>
-    <div className={styleSheet.Modal}
-    style={{
-      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-      opacity: props.show ? '1' : '0'
-    }}>
-      {props.children}
-    </div>
-  </Aux>
-  
-);
+class Modal extends Component{
+  shouldComponentUpdate(nextProps , nextState){
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  componentWillUpdate(){
+    console.log('[Modal will update]');
+  }
+  
+  render(){
+    return(
+      <Aux>
+        <BackDrop show={this.props.show} clicked={this.props.modalClosed}/>
+        <div className={styleSheet.Modal}
+        style={{
+          transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: this.props.show ? '1' : '0'
+        }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  };
+}
+export default Modal;
